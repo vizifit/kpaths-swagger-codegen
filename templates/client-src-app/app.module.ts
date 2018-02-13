1afcc30849958e266c034b0858378d11c8300873
Tony Henderson <info@vizifit.com>
2018-02-12 20:52:20 -0800

    Wrapped up current version
    Fixed a lot of issues with generating script for ang

---
+++ templates/client-src-app/app.module.ts
@@ -0,0 +1,20 @@
+import { HeaderModule } from './modules/header/header.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HeaderModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
